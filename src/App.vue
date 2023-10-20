b025efe994c5892d5c0eb613448c8635d8d8ca00
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2023-10-20 10:51:34 +0200

    feat : fetch some information without cloud

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,21 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/history">History</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
+  display: flex;
   width: 100%;
   font-size: 12px;
   text-align: center;
@@ -57,22 +41,6 @@ nav a:first-of-type {
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
   nav {
     text-align: left;
     margin-left: -1rem;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,50 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, ref } from 'vue'
+
+let loading = ref(false)
+let status = ref([])
+let settings = ref([])
+
+onMounted(async () => {
+  loading.value=true
+  const statusResponse = await fetch('http://192.168.1.100/status', {
+  })
+  status.value = await statusResponse.json()
+  console.log(status.value)
+
+  const settingsResponse = await fetch('http://192.168.1.100/settings', {
+  })
+  settings.value = await settingsResponse.json()
+  console.log(settings.value)
+  loading.value=false
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div v-if="loading">
+    <p>loading...</p>
+  </div>
+  <div v-else>
+    <div v-if="status && settings">
+      <main>
+        <div v-if="status.wifi_sta">
+          <p>Connectée sur : {{ status.wifi_sta.ssid  ? status.wifi_sta.ssid : 'loading...' }}</p>
+          <p>IP : {{ status.wifi_sta.ip  ? status.wifi_sta.ip : 'loading...' }}</p>
+        </div>
+        <div v-if="status.update && !status.update.new_version">
+          <p>A jour : OUI</p>
+        </div>
+        <div v-else>
+          <p>A jour : NON</p>
+        </div>
+        <div v-if="status.cloud">
+          <p>Connecté au cloud : {{ status.cloud.connected  ? status.cloud.connected : 'loading...' }}</p>
+          <!--      <p>A jour : {{ status.wifi_sta.ip  ? status.wifi_sta.ip : 'loading...' }}</p>-->
+        </div>
+      </main>
+    </div>
+    <div>
+      <p>no data</p>
+    </div>
+  </div>
 </template>
